58d3416470dfe46b59df0899e16d74b17c2239d7
Fabian Huch <huch@in.tum.de>
2022-09-29 15:59:06 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Isabelle Tools Collection
+This is a collection of maintained [Isabelle](https://isabelle.in.tum.de) add-on tools.
+Most are Isabelle/Scala components that are either very niche, too experimental,
+or not stable enough, to have made it into the distribution.
+
+Each tool resides in its own repository.
+This page merely gives an overview,
+and provides tooling to trigger ci pipelines on Isabelle changes.
+
+- [Linter](https://github.com/isabelle-prover/isabelle-linter)
---
+++ src/ci_notify.scala
@@ -0,0 +1,56 @@
+/* Author: Fabian Huch, TU Muenchen
+
+Build job to trigger ci pipelines.
+ */
+package isabelle.tools_collection
+
+
+import isabelle.*
+
+import java.net.{HttpURLConnection, URL}
+import java.io.{BufferedReader, IOException, InputStreamReader, OutputStreamWriter}
+
+
+object CI_Notify {
+  def post(url: URL, params: Map[String, String], body: String): String =
+    try {
+      val conn = url.openConnection().asInstanceOf[HttpURLConnection]
+      conn.setRequestMethod("POST")
+
+      conn.setDoOutput(true)
+      params.foreach { case (param, value) => conn.setRequestProperty(param, value) }
+      val writer = new OutputStreamWriter(conn.getOutputStream)
+      writer.write(body)
+      writer.flush()
+
+      File.read_stream(conn.getInputStream)
+    } catch {
+      case _: IOException => error("Could not read from " + url.toString)
+    }
+
+  def dispatch: CI_Build.Result = {
+    try {
+      post(
+        new URL("https://api.github.com/repos/isabelle-prover/isabelle-linter/dispatches"),
+        Map(
+          "Accept" -> "application/vnd.github+json",
+          "Authorization" -> ("Bearer " + System.getProperty("github.dispatcher_token"))),
+        "{\"event_type\":\"isabelle-update\"}")
+      CI_Build.Result.ok
+
+    } catch {
+      case ERROR(msg) =>
+        println("POST request failed: " + quote(msg))
+        CI_Build.Result.error
+    }
+  }
+
+  val ci_notify = CI_Build.Job(
+    "notify", "notifies external ci pipelines",
+    CI_Build.Profile(1, 1, false),
+    CI_Build.Build_Config(documents = false, clean = false, post_hook = (_, _) => dispatch)
+  )
+}
+
+class CI_Builds extends Isabelle_CI_Builds(
+  CI_Notify.ci_notify)
